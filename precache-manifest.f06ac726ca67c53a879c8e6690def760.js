8bfa0e9a4d96aed5c13fd8f66c37b0fb52249c3c
Argentum32 <61469351+Argentum32@users.noreply.github.com>
2020-10-18 00:30:39 +0300

    Updates

---
+++ precache-manifest.f06ac726ca67c53a879c8e6690def760.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07dbe9ce3bbaacff253f0df262087697",
+    "url": "/library/index.html"
+  },
+  {
+    "revision": "75b4fcf6d2ae3a25c60d",
+    "url": "/library/static/css/main.97242c3c.chunk.css"
+  },
+  {
+    "revision": "65cfda3ca7cc34e4a63b",
+    "url": "/library/static/js/2.88c2dcd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/library/static/js/2.88c2dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b4fcf6d2ae3a25c60d",
+    "url": "/library/static/js/main.74a01c5c.chunk.js"
+  },
+  {
+    "revision": "09567d17720e24bfc93b",
+    "url": "/library/static/js/runtime-main.7eb54568.js"
+  },
+  {
+    "revision": "67a7f3b35b075ac4a8b434cce0808384",
+    "url": "/library/static/media/lamp.67a7f3b3.png"
+  },
+  {
+    "revision": "471eb4dc7b921602f85a4332cd2b9e7d",
+    "url": "/library/static/media/like.471eb4dc.svg"
+  },
+  {
+    "revision": "3d02a97331e480356428030f6229768d",
+    "url": "/library/static/media/liked.3d02a973.svg"
+  }
+]);
